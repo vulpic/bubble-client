309563b98dc29ded6cd38766097a87f54ba0b739
Vulpic <66442608+vulpic@users.noreply.github.com>
2023-03-04 19:51:32 -0600

    fix bugs

--- src/commands/contextUwuify.ts
+++ src/commands/contextUwuify.ts
@@ -2,6 +2,7 @@ import {
     ApplicationCommandType,
     ContextMenuCommandBuilder,
   } from "discord.js";
+import { client } from "..";
 import { MessageContextMenu } from "../types/interactions";
 import utils from "../util/utils";
 
@@ -9,6 +10,20 @@ const data: MessageContextMenu = {
   name: "UwUify",
   run: async (i) => {
     const message = i.targetMessage;
+    if (message.author == client.user) {
+        await i.reply({
+            content: "uwu",
+            ephemeral: true
+        })
+        return
+    }
+    if (message.content == "") {
+        await i.reply({
+            content: "No message content.",
+            ephemeral: true
+        })
+        return
+    }
     await message.reply({
       content: utils.uwuifier.uwuifySentence(message.content),
       allowedMentions: { parse: [] }
