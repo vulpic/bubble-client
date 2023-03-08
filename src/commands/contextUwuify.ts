959ba544a5053e519891ad439798f3f48441fad2
Vulpic <66442608+vulpic@users.noreply.github.com>
2023-03-07 20:36:15 -0600

    Fix contextUwuify.ts
    allows multiple newline chars, I didn't use replaceAll.

--- src/commands/contextUwuify.ts
+++ src/commands/contextUwuify.ts
@@ -1,7 +1,4 @@
-import {
-    ApplicationCommandType,
-    ContextMenuCommandBuilder,
-  } from "discord.js";
+import { ApplicationCommandType, ContextMenuCommandBuilder } from "discord.js";
 import { client } from "..";
 import { MessageContextMenu } from "../types/interactions";
 import utils from "../util/utils";
@@ -11,46 +8,50 @@ const data: MessageContextMenu = {
   run: async (i) => {
     const message = i.targetMessage;
     if (message.author == client.user) {
-        await i.reply({
-            content: "uwu",
-            ephemeral: true
-        })
-        return
+      await i.reply({
+        content: "uwu",
+        ephemeral: true,
+      });
+      return;
     }
     if (message.content == "") {
-        await i.reply({
-            content: utils.uwuifier.uwuifySentence("There is no message content!"),
-            ephemeral: true
-        })
-        return
+      await i.reply({
+        content: utils.uwuifier.uwuifySentence("There is no message content!"),
+        ephemeral: true,
+      });
+      return;
     }
 
-    const uwu = utils.uwuifier.uwuifySentence(message.content)
+    const uwu = utils.uwuifier.uwuifySentence(message.content);
     if (uwu == message.content) {
-        await i.reply({
-            content: utils.uwuifier.uwuifySentence("This message did not get changed through the filter!"),
-            ephemeral: true
-        })
-        return
+      await i.reply({
+        content: utils.uwuifier.uwuifySentence(
+          "This message did not get changed through the filter!"
+        ),
+        ephemeral: true,
+      });
+      return;
     }
     try {
-        await message.reply({
-            content: uwu,
-            allowedMentions: { parse: [] }
-            });
-        await i.reply({
-            content: utils.uwuifier.uwuifySentence("Message UwUified!"),
-            ephemeral: true
-        })
-        await i.deleteReply()
+      await message.reply({
+        content: uwu,
+        allowedMentions: { parse: [] },
+      });
+      await i.reply({
+        content: utils.uwuifier.uwuifySentence("Message UwUified!"),
+        ephemeral: true,
+      });
+      await i.deleteReply();
     } catch {
-        await i.reply({
-            content: utils.uwuifier.uwuifySentence("I do not have permissions to type here!"),
-            ephemeral: true
-        })
+      await i.reply({
+        content: utils.uwuifier.uwuifySentence(
+          "I do not have permissions to type here!"
+        ),
+        ephemeral: true,
+      });
     }
-},
-builder: new ContextMenuCommandBuilder()
+  },
+  builder: new ContextMenuCommandBuilder()
     .setName("UwUify")
     .setType(ApplicationCommandType.Message),
 };
